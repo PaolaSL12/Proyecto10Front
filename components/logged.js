87bf3803ccfc4de0f7c90a64cf6d9c9276008f94
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-04-01 13:54:51 +0200

    fix logged user fetch

--- components/logged.js
+++ components/logged.js
@@ -1,7 +1,7 @@
 export const getlogged = async () => {
     const logged = JSON.parse(localStorage.getItem("user"))
 
-    const res = await fetch(`http://localhost:3000/api/attendees/name/${logged.name}`);
+    const res = await fetch(`https://proyecto10-back-phi.vercel.app/api/attendees/name/${logged.name}`);
     const response = await res.json();
     
     const userAteendee = response[0]
